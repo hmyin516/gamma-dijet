fde52633f8bc47631787962cd32757717b4542dd
hmyin <ham.htetaungmyin@gmail.com>
2020-06-26 06:53:47 -0700

    cleaned code

--- scale_factor/docheckfrac_gammafixed.py
+++ scale_factor/docheckfrac_gammafixed.py
@@ -2,7 +2,7 @@ from ROOT import *
 import numpy as np
 
 doreweight = 0
-inputvar = "ntrk"
+inputvar = "bdt"
 mc = "sherpa_SF"
 var = inputvar
 ntrackall = TFile("ROOT/dijet_sherpa_py_forGamma.root")
@@ -19,11 +19,13 @@ def myText(x,y,text,color =1):
     pass
 
 bin = [0,50,100,150,200,300,400,500,600,800,1000,1200,1500,2000]
-for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
+for i in range(0,8):   #for only dijet event, start from jet pT>500 GeV
 #for i in range(13):	#for gamma+jet combined with dijet event, start from jet pT>0 GeV
     min = bin[i]
     max = bin[i+1]
 
+    #input variables
+    # sim
     dijet_quark = ntrackall.Get(str(min)+"_LeadingJet_Forward_Quark_"+inputvar)
     dijet_subjetquark = ntrackall.Get(str(min)+"_SubJet_Forward_Quark_"+inputvar)
 
@@ -39,58 +41,42 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     gamma_quark = gammasherpa.Get(str(min) + "_LeadingJet_Central_Quark_" + inputvar)
     gamma_gluon = gammasherpa.Get(str(min) + "_LeadingJet_Central_Gluon_" + inputvar)
 
-
-
+    # data 
     dijet_data1 = ntrackall3.Get(str(min)+"_LeadingJet_Forward_Data_"+inputvar)
     dijet_data2 = ntrackall3.Get(str(min)+"_LeadingJet_Central_Data_"+inputvar)
-
     dijet_data3 = ntrackall3.Get(str(min)+"_SubJet_Forward_Data_" + inputvar)
     dijet_data4 = ntrackall3.Get(str(min)+"_SubJet_Central_Data_" + inputvar)
 
     gamma_data = gammadata.Get(str(min)+"_LeadingJet_Central_Data_"+inputvar)
 
 
-
+    #adding data together
     dijet_data1.Add(dijet_data2)
     dijet_data3.Add(dijet_data4)
-    #Leading jets only,no matter forward ro central
-    for a in range(60):
-		if np.isnan(dijet_quark.GetBinContent(a)):
-			dijet_quark.SetBinContent(a,0)
-		if np.isnan(dijet_quark2.GetBinContent(a)):
-			dijet_quark2.SetBinContent(a,0)
-
+   
+    # checking for nan bins in dijet sample (quick fix) 
+    def checkNan(jet1,jet2,numRange):
+        for a in range(numRange):
+		    if np.isnan(jet1.GetBinContent(a)):
+			    jet1.SetBinContent(a,0)
+		    if np.isnan(jet2.GetBinContent(a)):
+			    jet2.SetBinContent(a,0)
+ 
+    checkNan(dijet_quark,dijet_quark2,60)
+    checkNan(dijet_gluon,dijet_gluon2,60)
+    checkNan(dijet_subjetquark,dijet_subjetquark2,60)
+    checkNan(dijet_subjetgluon,dijet_subjetgluon2,60)
+    
+    #adding dijet and subjet together
     dijet_quark.Add(dijet_quark2)
-   # lower_quark = dijet_quark
-
-
-    for b in range(60):
-		if np.isnan(dijet_gluon.GetBinContent(b)):
-			dijet_gluon.SetBinContent(b,0)
-		if np.isnan(dijet_gluon2.GetBinContent(b)):
-			dijet_gluon2.SetBinContent(b,0)
-
     dijet_gluon.Add(dijet_gluon2)
-   # lower_gluon = dijet_gluon
-
-    for a in range(60):
-        if np.isnan(dijet_subjetquark.GetBinContent(a)):
-            dijet_subjetquark.SetBinContent(a,0)
-        if np.isnan(dijet_subjetquark2.GetBinContent(a)):
-            dijet_subjetquark2.SetBinContent(a,0)
-
     dijet_subjetquark.Add(dijet_subjetquark2)
-
-    for a in range(60):
-        if np.isnan(dijet_subjetgluon.GetBinContent(a)):
-            dijet_subjetgluon.SetBinContent(a,0)
-        if np.isnan(dijet_subjetgluon2.GetBinContent(a)):
-            dijet_subjetgluon2.SetBinContent(a,0)
     dijet_subjetgluon.Add(dijet_subjetgluon2)
     dijet_quark.Add(dijet_subjetquark)
     dijet_gluon.Add(dijet_subjetgluon)
     dijet_data1.Add(dijet_data3)
-    #reassigning to variables
+    
+    #reassigning to variables for convienece
     higher_quark = gamma_quark
     higher_gluon = gamma_gluon
     lower_quark = dijet_quark
@@ -98,51 +84,33 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     higher_data = gamma_data
     lower_data = dijet_data1
 
-
-
     #add leading and subleading jet from only dijet event together,
     #note that for gammajet+dijet event, we need to add leading jet from gammajet and leading jet from dijet sample together
-   # higher_data.Add(higher_data2)
-   # lower_data.Add(lower_data2)
-   # higher_quark.Add(higher_quark2)
-   # higher_gluon.Add(higher_gluon2)
-
     ToT_Fq2 = 0.
     ToT_Fg2 = 0.
-
     ToT_Cq2 =0.
     ToT_Cg2 = 0.
+
     for j in range(1,lower_quark.GetNbinsX()+1):
 		ToT_Fq2+=higher_quark.GetBinContent(j)  #dijetquark
 		ToT_Cq2+=lower_quark.GetBinContent(j) #gammaquark
-		ToT_Fg2+=higher_gluon.GetBinContent(j) #dijetlguon
+		ToT_Fg2+=higher_gluon.GetBinContent(j) #dijetgluon
 		ToT_Cg2+=lower_gluon.GetBinContent(j)#gammagluon
 
-
-
-
-    # calculate the fraction of forward(higher) / central(lower) quark or gluon jet
-    print("checking fraction")
+    # calculate the fraction of each sample 
     if ((ToT_Fg2+ToT_Fq2) != 0):
 		fg=ToT_Fg2/(ToT_Fg2+ToT_Fq2)
 		cg=ToT_Cg2/(ToT_Cq2+ToT_Cg2)
     else:
 		continue
-
     fq=1.-fg
     cq=1.-cg
-
-    print(fg)
-    print(cg)
-    print(fq)
-    print(cq)
-
 
     c = TCanvas("c","c",500,500)
     if (doreweight):
         for i in range(1,higher_quark.GetNbinsX()+1):
             if (lower_quark.GetBinContent(i) > 0 and lower_gluon.GetBinContent(i) > 0):
-                #print i,higher_quark.GetBinContent(i)/lower_quark.GetBinContent(i),higher_gluon.GetBinContent(i)/lower_gluon.GetBinContent(i)
+                
                 factor_gluon = higher_gluon.GetBinContent(i)/lower_gluon.GetBinContent(i)
                 factor_quark = higher_quark.GetBinContent(i)/lower_quark.GetBinContent(i)
                 lower_quark.SetBinContent(i,lower_quark.GetBinContent(i)*factor_quark)
@@ -151,7 +119,6 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
                 pass
             pass
         pass
-
 
     if (lower_quark.Integral() != 0):
         lower_quark.Scale(1./lower_quark.Integral())
@@ -167,23 +134,24 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
         higher_data.Scale(1./higher_data.Integral())
 
 
-
+    #cloning for bin size
     higher = higher_quark.Clone("")
     lower = higher_quark.Clone("")
-
+    #setting bin content for cloned bins
     for i in range(1,higher.GetNbinsX()+1):
         higher.SetBinContent(i,fg*higher_gluon.GetBinContent(i)+fq*higher_quark.GetBinContent(i))
         lower.SetBinContent(i,cg*lower_gluon.GetBinContent(i)+cq*lower_quark.GetBinContent(i))
         pass
-
-    #Now, let's solve.
 
     quark = higher_quark.Clone("")
     gluon = higher_quark.Clone("")
     quark_data = higher_data.Clone("")
     gluon_data = higher_data.Clone("")
 
-    #Matrix method here
+
+    # Matrix method 
+
+    # Simulation 
     for i in range(1,higher.GetNbinsX()+1):
         F = higher.GetBinContent(i)
         C = lower.GetBinContent(i)
@@ -192,8 +160,8 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
             G = (C*fq-F*cq)/(cg*fq-fg*cq)
             quark.SetBinContent(i,Q)
             gluon.SetBinContent(i,G)
-            #print "   ",i,G,higher_gluon.GetBinContent(i),lower_gluon.GetBinContent(i)
         pass
+    # Data
     for i in range(1,higher_data.GetNbinsX()+1):
         F = higher_data.GetBinContent(i)
         C = lower_data.GetBinContent(i)
@@ -204,6 +172,7 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
             gluon_data.SetBinContent(i,G)
         pass
 
+    # plotting
     gPad.SetLeftMargin(0.15)
     gPad.SetTopMargin(0.05)
     gPad.SetBottomMargin(0.15)
@@ -226,60 +195,41 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     gStyle.SetOptStat(0)
     ######################## for ratio plot
     c.Divide(2,1)
+    # sets parameters for graph
+    def setTopBottom(q,pad1,pad2,pad3,pad4,color,mode,size,tickx,ticky,lMargin,rMargin,bMargin,BorderMode):  
+        q.SetPad(pad1,pad2,pad3,pad4)
+        q.SetFillColor(color)
+        q.SetBorderMode(mode)
+        q.SetTickx(tickx)
+        q.SetTicky(ticky)
+        q.SetLeftMargin(lMargin)
+        q.SetRightMargin(rMargin)
+        q.SetBottomMargin(bMargin)
+        q.SetFrameBorderMode(BorderMode)
 
     top = c.cd(1)
-    top.SetPad(0.0,0.0,1.0,1.0)
-    top.SetFillColor(0)
-    top.SetBorderMode(0)
-    top.SetBorderSize(2)
-    top.SetTickx(1)
-    top.SetTicky(1)
-    top.SetLeftMargin(0.14)
-    top.SetRightMargin(0.055)
-    top.SetBottomMargin(0.3)#0.25
-    top.SetFrameBorderMode(0)
-    #top.SetLogy(1)
-
+    setTopBottom(top,0.0,0.0,1.0,1.0,0,0,2,1,1,0.14,0.055,0.3,0)
     bot = c.cd(2)
-    bot.SetPad(0.0,0.0,1.0,0.3)
-    bot.SetFillColor(0)
-    bot.SetBorderMode(0)
-    bot.SetBorderSize(2)
-    bot.SetTickx(1)
-    bot.SetTicky(1)
-    bot.SetLeftMargin(0.14)
-    bot.SetRightMargin(0.055)
     bot.SetTopMargin(0.045)
-    bot.SetBottomMargin(0.4)
-    bot.SetFrameBorderMode(0)
+    setTopBottom(bot,0.0,0.0,1.0,0.3,0,0,2,1,1,0.14,0.055,0.4,0)
 
+    # set parameters for graph lines/markers
+    def graphMarkers(variable,markColor,lineColor,markSize,markStyle):
+        variable.SetMarkerColor(markColor)
+        variable.SetLineColor(lineColor)
+        variable.SetMarkerSize(markSize)
+        variable.SetMarkerStyle(markStyle)
+        
     quark.SetTitle("")
     quark.GetXaxis().SetTitle(var)
     quark.GetYaxis().SetTitle("Normalized to unity")
     quark.GetYaxis().SetNdivisions(505)
     quark.GetYaxis().SetRangeUser(-0.01,quark.GetMaximum()*1.5)
-    #quark.GetYaxis().SetRangeUser(-0.01,0.05)
-    quark.SetMarkerColor(8)
-    quark.SetLineColor(8)
-    quark.SetMarkerSize(0.5)
-    quark.SetMarkerStyle(20)
 
-
-    quark_data.SetMarkerColor(1)
-    quark_data.SetLineColor(1)
-    quark_data.SetMarkerSize(0.8)
-    quark_data.SetMarkerStyle(29)
-
-    higher_quark.SetMarkerColor(2)
-    higher_quark.SetLineColor(2)
-    higher_quark.SetMarkerSize(0.5)
-    higher_quark.SetMarkerStyle(21)
-
-    lower_quark.SetMarkerColor(4)
-    lower_quark.SetLineColor(4)
-    lower_quark.SetMarkerSize(0.5)
-    lower_quark.SetMarkerStyle(22)
-
+    graphMarkers(quark,8,8,0.5,20)
+    graphMarkers(quark_data,1,1,0.8,29)
+    graphMarkers(higher_quark,2,2,0.5,21)
+    graphMarkers(lower_quark,4,4,0.5,22)
 
     quark_ratio.SetTitle("")
     quark_ratio.GetYaxis().SetRangeUser(0.7,1.3)
@@ -290,7 +240,6 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     quark_ratio.GetXaxis().SetLabelOffset(0.03)
     quark_ratio.GetYaxis().SetTitleSize(0.1)
     quark_ratio.GetYaxis().SetTitleOffset(0.5)
-    #quark_ratio.GetYaxis().SetLabelSize(0.2)
     quark_ratio.GetYaxis().SetLabelOffset(0.01)
 
     top.cd()
@@ -299,24 +248,23 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
 		quark_data.Draw("same")
     if "MC" in mc:
          higher_quark.Draw("same")
-       # lower_quark.Draw("same")
-		#higher_data.Draw("same")
-		#lower_data.Draw("same")
     if "bdt" in inputvar:
         leg = TLegend(0.75,0.57,0.94,0.67)
         line = TLine(-0.8,1,0.7,1)
     if "ntrk" in inputvar:
         leg = TLegend(0.55,0.55,0.82,0.65)
         line = TLine(0,1,60,1)
-    else:
-        leg = TLegend(0.55,0.55,0.82,0.65)
-        line = TLine(0,1,60,1)
-    leg.SetTextFont(42)
-    leg.SetFillColor(0)
-    leg.SetBorderSize(0)
-    leg.SetFillStyle(0)
-    leg.SetNColumns(1)
+    
+    # set parameters for legend
+    def legFormat(textFont,FillColor,BorderSize,FillStyle,NColumns):
+        leg.SetTextFont(textFont)
+        leg.SetFillColor(FillColor)
+        leg.SetBorderSize(BorderSize)
+        leg.SetFillStyle(0)
+        leg.SetNColumns(NColumns)
+    legFormat(42,0,0,0,1)
     leg.AddEntry(quark,"Extracted quark (mc)","p")
+
     if "SF" in mc:
         myText(0.18,0.84,"#it{#bf{#scale[1.8]{#bf{ATLAS} Internal}}}")
         leg.AddEntry(quark_data,"Extracted quark (data)","p")
@@ -328,44 +276,21 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     myText(0.18,0.80,"#bf{#scale[1.5]{#sqrt{s} = 13 TeV}}")
     myText(0.18,0.75,"#bf{#scale[1.5]{pT range: "+str(min)+" - "+str(max)+" GeV}}")
 
-
-   # line = TLine(0.,1,60,1)
-   # line = TLine(-0.8,1,0.7,1)
-    #line = TLine(0.,1,0.4,1)
-
     bot.cd()
     quark_ratio.Draw()
     line.Draw("same")
-    #c.Print("./plots_bdt/quark_"+str(min)+"_"+str(doreweight)+"_"+mc+"_"+var+"_fc.pdf")
+
     c.Print("./plots/gamma+/2d1g/plots_"+var+"/quark/quark_"+str(min)+"_"+str(doreweight)+"_"+mc+"_"+var+".pdf")
-    #c.Print("./plots/gamma+/truthMC/plots_"+var+"/quark/truth_MC_quark_"+str(min)+".pdf")
 
     gluon.SetTitle("")
     gluon.GetXaxis().SetTitle(var)
     gluon.GetYaxis().SetTitle("Normalized to unity")
     gluon.GetYaxis().SetNdivisions(505)
-    #gluon.GetYaxis().SetRangeUser(-0.01,0.05)
     gluon.GetYaxis().SetRangeUser(-0.01,gluon.GetMaximum()*1.75)
-    gluon.SetMarkerColor(8)
-    gluon.SetLineColor(8)
-    gluon.SetMarkerSize(0.5)
-    gluon.SetMarkerStyle(20)
-
-    gluon_data.SetMarkerColor(1)
-    gluon_data.SetLineColor(1)
-    gluon_data.SetMarkerSize(0.8)
-    gluon_data.SetMarkerStyle(29)
-
-    higher_gluon.SetMarkerColor(2)
-    higher_gluon.SetLineColor(2)
-    higher_gluon.SetMarkerSize(0.5)
-    higher_gluon.SetMarkerStyle(21)
-
-    lower_gluon.SetMarkerColor(4)
-    lower_gluon.SetLineColor(4)
-    lower_gluon.SetMarkerSize(0.5)
-    lower_gluon.SetMarkerStyle(22)
-
+    graphMarkers(gluon,8,8,0.5,20)
+    graphMarkers(gluon_data,1,1,0.8,29)
+    graphMarkers(higher_gluon,2,2,0.5,21)
+    graphMarkers(lower_gluon,4,4,0.5,22)
 
     gluon_ratio.SetTitle("")
     gluon_ratio.GetYaxis().SetRangeUser(0.7,1.3)
@@ -376,9 +301,7 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     gluon_ratio.GetXaxis().SetLabelOffset(0.03)
     gluon_ratio.GetYaxis().SetTitleSize(0.1)
     gluon_ratio.GetYaxis().SetTitleOffset(0.5)
-    #gluon_ratio.GetYaxis().SetLabelSize(0.2)
     gluon_ratio.GetYaxis().SetLabelOffset(0.01)
-
 
     top.cd()
     gluon.Draw()
@@ -386,21 +309,16 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
         gluon_data.Draw("same")
     if "MC" in mc:
         higher_gluon.Draw("same")
-    #lower_gluon.Draw("same")
-    #higher_data.Draw("same")
-    #lower_data.Draw("same")
+
     if "bdt" in inputvar:
         leg = TLegend(0.15,0.55,0.42,0.65)
         line = TLine(-0.8,1,0.7,1)
     if "ntrk" in inputvar:
         leg = TLegend(0.55,0.55,0.82,0.65)
         line = TLine(0,1,60,1)##0.6,0.5,0.9,0.7
-    #leg = TLegend(0.6,0.5,0.9,0.7) ##0.6,0.5,0.9,0.7
-    leg.SetTextFont(42)
-    leg.SetFillColor(0)
-    leg.SetBorderSize(0)
-    leg.SetFillStyle(0)
-    leg.SetNColumns(1)
+  
+    legFormat(42,0,0,0,1)
+    
     leg.AddEntry(gluon,"Extracted gluon (mc)","p")
     if "SF" in mc:
         leg.AddEntry(gluon_data,"Extracted gluon (data)","p")
@@ -408,19 +326,12 @@ for i in range(1,7):   #for only dijet event, start from jet pT>500 GeV
     if "MC" in mc:
         myText(0.18,0.84,"#it{#bf{#scale[1.8]{#bf{ATLAS} Simulation Internal}}}")
         leg.AddEntry(higher_gluon,"gamma gluon (mc)","p")
-    #leg.AddEntry(lower_gluon,"lower gluon (mc)","p")
     leg.Draw()
 
     myText(0.18,0.80,"#bf{#scale[1.5]{#sqrt{s} = 13 TeV}}")
     myText(0.18,0.75,"#bf{#scale[1.5]{pT range: "+str(min)+" - "+str(max)+" GeV}}")
 
-
-   # line = TLine(0.,1,60,1)
-   # line = TLine(-0.8,1,0.7,1)
-    #line = TLine(0.,1,0.4,1)
-
     bot.cd()
     gluon_ratio.Draw()
     line.Draw("same")
-    #c.Print("./plots/gamma+/truthMC/plots_"+var+"/gluon/truth_MC_gluon_"+str(min)+".pdf")
-   c.Print("./plots/gamma+/2d1g/plots_"+var+"/gluon/gluon_"+str(min)+"_"+str(doreweight)+"_"+mc+"_"+var+".pdf")
+    c.Print("./plots/gamma+/2d1g/plots_"+var+"/gluon/gluon_"+str(min)+"_"+str(doreweight)+"_"+mc+"_"+var+".pdf")
